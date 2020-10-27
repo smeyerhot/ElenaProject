5c2238e983f949b2eea274d2b78dd0ad066e29c6
Louis <louismartin@umass.edu>
2020-10-27 18:52:44 -0400

    Map state now has a path field. When two points are clicked, it draws a purple line on the map between those two points.

--- client/src/components/map.js
+++ client/src/components/map.js
@@ -1,11 +1,12 @@
-import {Map, TileLayer, Marker, Popup} from "react-leaflet"
+import {Map, TileLayer, Marker, Popup, Polyline} from "react-leaflet"
 
 export default class MyMap extends React.Component {
   constructor() {
     super();
     this.state = {
       position: [42.3868 , -72.5301],
-      markers: []
+      markers: [],
+      path: []
     };
   }
 
@@ -17,12 +18,32 @@ export default class MyMap extends React.Component {
   //   });
   // }
   addMarker = (e) => {
-    let {markers} = this.state
+    let {markers, path} = this.state
     if(markers.length === 2){
       markers = [];
+      path = []
     }
-    markers.push(e.latlng)
-    this.setState({markers})
+    markers.push(e.latlng);
+    let node;
+    if(path.length == 0){
+      node = {
+        from_lat: e.latlng.lat,
+        from_long: e.latlng.lng,
+        id: path.length,
+        to_lat: '',
+        to_long: ''
+      }
+      path.push(node);
+    }
+    else{
+      node = path[0];
+      node.to_lat = e.latlng.lat;
+      node.to_long = e.latlng.lng;
+    }
+    
+
+    
+    this.setState({markers, path});
   }
   // function to set a new center of map position
   // setPos(pos){
@@ -30,7 +51,15 @@ export default class MyMap extends React.Component {
   //   position = [pos.latitude, pos.longitude];
   //   this.setState({position})
   // }
-  
+  renderLine(){
+    let {path, update} = this.state
+    update = true;
+    this.setState({update});
+    if(path.length > 0){
+      return <Polyline color = "purple" positions = {path}></Polyline>
+    }
+    
+  }
   render() {
     
     return (
@@ -46,13 +75,22 @@ export default class MyMap extends React.Component {
         />
         {this.state.markers.map((position, idx) => 
           <Marker key={`marker-${idx}`} position={position}>
-            {console.log(idx)}
               <Popup>
               <span>This is your {idx == 0 ? 'start' : 'end'} point. </span>
               </Popup>
           
         </Marker>
+        
         )}
+        {this.state.path.map(({id, from_lat, from_long, to_lat, to_long }) => {
+          if(to_lat.length !== 0){
+            let line = [[from_lat, from_long], [to_lat, to_long]];
+            return <Polyline key = {id} color = "purple" positions = {line}></Polyline>
+          }
+          
+        })}
+        
+        
       </Map>
     );
   }
