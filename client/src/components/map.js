623891c65d8eba90ecd38930cfdbd286a58a96c1
Louis <louismartin@umass.edu>
2020-10-27 13:02:15 -0400

    Refactored myMap into react component. Map now allows users to click a start and end point for their chosen route.

--- client/src/components/map.js
+++ client/src/components/map.js
@@ -1,14 +1,44 @@
-import {Map, TileLayer} from "react-leaflet"
+import {Map, TileLayer, Marker, Popup} from "react-leaflet"
 
-export default function MyMap() {
+export default class MyMap extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      position: [42.3868 , -72.5301],
+      markers: []
+    };
+  }
 
-      return (
-        <Map center={[42.3868 , -72.5301]} zoom={14} style = {{height: '100vh', width: '100%'}} >
-          <TileLayer
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
-          />
-        </Map>
-      );
+  addMarker = (e) => {
+    let {markers} = this.state
+    if(markers.length === 2){
+      markers = [];
+    }
+    markers.push(e.latlng)
+    this.setState({markers})
+  }
+
+  render() {
+    return (
+      <Map 
+        center={this.state.position} 
+        onClick={this.addMarker}
+        zoom={13} 
+        >
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+        />
+        {this.state.markers.map((position, idx) => 
+          <Marker key={`marker-${idx}`} position={position}>
+            {console.log(idx)}
+              <Popup>
+              <span>This is your {idx == 0 ? 'start' : 'end'} point. </span>
+              </Popup>
+          
+        </Marker>
+        )}
+      </Map>
+    );
+  }
 }
-
