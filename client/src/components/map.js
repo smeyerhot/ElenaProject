03f89182103c7af072987229faa1395913aed894
Louis <louismartin@umass.edu>
2020-11-09 18:03:09 -0500

    Restrctured map to be a functional component with hooks instead of class based.

--- client/src/components/map.js
+++ client/src/components/map.js
@@ -1,110 +1,110 @@
 import {Map, TileLayer, Marker, Popup, Polyline} from "react-leaflet"
 import {regIcon, startIcon, endIcon} from "./icons.js"
-export default class MyMap extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      start: [42.3868, -72.5301],
-      end: [42.4007,-72.5162],
-      position: [42.3868 , -72.5301],
-      markers: [],
-      path: [],
-      grid: []
-    };
-  }
-  componentDidMount(){
-    this.getGrid();
-    this.setState({state: this.state})
-  }
+import {useState, useEffect} from 'react'
+
+export default function MyMap () {
+    const [nodeCount, setNodeCount] = useState(0);
+    const [start, setStart] = useState(null);
+    const [end, setEnd] = useState(null);
+    const [position, setPosition] = useState([42.3868 , -72.5301]);
+    const [path, setPath] = useState([]);
+    const [markers, setMarkers] = useState([]);
+    const [grid, setGrid] = useState([]);
+  
   //testing for if we want to implement users current location
-  // componentDidMount(){
-
-  //   navigator.geolocation.getCurrentPosition((position) => {
-  //     this.setPos(position.coords);
+  // useEffect(() =>{
+  //  navigator.geolocation.getCurrentPosition((position) => {
+  //     setPosition(position.coords);
   //   });
+  //})
+
+  //   
   // }
-  async getGrid(){
-    let response = await fetch('http://localhost:5000/api/coords', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      credentials: 'include',
-      body: JSON.stringify({
-        "start": {
-          "lat": 42.3868,
-          "long": -72.5301
-        },
-        "end": {
-            "lat": 42.4007,
-            "long": -72.5162
+    
+
+    useEffect(()=> {
+        if(nodeCount === 1){
+            setStart(markers[0]);
         }
-      }),
-    }).then(async data =>  {
-      let body = await data.json();
-      // body.then(points => {
-      //   let {grid} = this.state.grid;
-
-      //   console.log(points);
-      //   grid = points;
-      //   this.setState({grid});
-      // }
-      // )})
-      let {markers} = this.state;
-      markers.push(this.state.start);
-      markers.push(this.state.end);
-      let i = 0;
-      for (let data of body.grid){
-        if(i%3 === 0){
-          let pos = [data.lat, data.long];
-          markers.push(pos);
+        if(nodeCount === 2){
+            setEnd(markers[1]);
         }
-        ++i;
-      }
-      this.setState({markers});
-    })
-  }
+    }, [markers, nodeCount]);
+
+    useEffect(() => {
+
+
+        
+        if(grid.length === 0){
+            async function getGrid(){
+                let response = await fetch('http://localhost:5000/api/coords', {
+                    method: 'POST',
+                    headers: {
+                      'Accept': 'application/json',
+                      'Content-Type': 'application/json'
+                    },
+                    credentials: 'include',
+                    body: JSON.stringify({
+                      "start": {
+                        "lat": 42.3868,
+                        "long": -72.5301
+                      },
+                      "end": {
+                          "lat": 42.4007,
+                          "long": -72.5162
+                      }
+                    }),
+                  }).then(async data =>  {
+                    let body = await data.json();
+                    // body.then(points => {
+                    //   let {grid} = this.state.grid;
+              
+                    //   console.log(points);
+                    //   grid = points;
+                    //   this.setState({grid});
+                    // }
+                    // )})
+                    let grid = []
+                    let i = 0;
+                    for (let data of body.grid){
+                      if(i%3 === 0){
+                          
+                        let pos = {lat: data.lat, lng: data.long};
+                        console.log(pos);
+                        setGrid(grid => [...grid, pos]);
+                      }
+                      ++i;
+                    }
+                  })
+            }
+            getGrid();
+        }
+        
+    }, []);
   
-
-  addMarker = (e) => {
-    let {markers, path} = this.state;
-    if(markers.length === 2){
-      markers = [];
-      path = []
+    function handleClick(e){
+        if(nodeCount <2){
+            setMarkers(markers => [...markers, e.latlng]);
+            setPath(path => [...path, e.latlng]);
+            setNodeCount(nodeCount +1);
+        }
+        else{
+            setMarkers([]);
+            setPath([]);
+            setNodeCount(0);
+        }
+        
+           
     }
-    let node;
-    if(path.length == 0){
-      node = {
-        from_lat: e.latlng.lat,
-        from_long: e.latlng.lng,
-        id: path.length,
-        to_lat: '',
-        to_long: ''
-      }
-      path.push(node);
-    }
-    else{
-      node = path[0];
-      node.to_lat = e.latlng.lat;
-      node.to_long = e.latlng.lng;
-    } 
-    this.setState({markers, path});
-  }
 
-  
-  // function to set a new center of map position
-  // setPos(pos){
-  //   let {position} = this.state;
-  //   position = [pos.latitude, pos.longitude];
-  //   this.setState({position})
-  // }
-
-  render() {
+    
     return (
+        
       <Map 
-        center={this.state.position} 
-        onClick={this.addMarker}
+        center={position} 
+        onClick={(e) => { 
+            handleClick(e);
+        }}
         zoom={14} 
         style = {{height: '84vh', width: '100%'}}
         >
@@ -112,50 +112,33 @@ export default class MyMap extends React.Component {
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
         />
-        {this.state.markers.map((position, idx) => {
-          let marker = null
-          if(position[0] === this.state.start[0] && position[1] === this.state.start[1]){
-            marker = <Marker key={`marker-${idx}`} position={position} icon = {startIcon}>
-                  <Popup>
-                  <span>This is your node {idx} </span>
-                  </Popup>
-              
-            </Marker>
-          }
-          else if(position[0] === this.state.end[0] && position[1] === this.state.end[1]){
-            marker = <Marker key={`marker-${idx}`} position={position} icon = {endIcon}>
-                  <Popup>
-                  <span>This is your node {idx} </span>
-                  </Popup>
-              
-            </Marker>
 
-          }
-          else{
-            console.log(idx);
-            marker =<Marker key={`marker-${idx}`} position={position} icon = {regIcon}>
+        {markers.map((position, idx) => {
+          return <Marker key={`marker-${idx}`} position={position}>
                   <Popup>
                   <span>This is your node {idx} </span>
                   </Popup>
               
             </Marker>
-          }
-          return marker;
-            
-        }
-        
-        
-        )}
-        {this.state.path.map(({id, from_lat, from_long, to_lat, to_long }) => {
-          if(to_lat.length !== 0){
-            let line = [[from_lat, from_long], [to_lat, to_long]];
-            return <Polyline key = {id} color = "purple" positions = {line}></Polyline>
-          }
-          
+        })}
+        {path.map((position, idx) => {
+            if(idx < path.length-1){
+                let line = [[position.lat, position.lng], [path[idx+1].lat, path[idx+1].lng]];
+                return <Polyline key = {idx} color = "purple" positions = {line}></Polyline>
+            }
+        })}
+        {grid.map((position, idx) => {
+            console.log(position);
+          return <Marker key={`marker-${idx}`} position={position}>
+                  <Popup>
+                  <span>This is your node {idx} </span>
+                  </Popup>
+              
+            </Marker>
         })}
 
-        
       </Map>
     );
-  }
-}
+    }
+
+    
