bea65e90ec6c564fd62974ce778f06b1e776ab9a
Louis <louismartin@umass.edu>
2020-11-17 12:44:29 -0500

    Finished front end integration between user map clicking input and the input fields.

--- client/src/components/mapInput.js
+++ client/src/components/mapInput.js
@@ -1,30 +1,41 @@
 import {Map, TileLayer, Marker, Popup, Polyline} from "react-leaflet"
+import {useState, useEffect} from 'react'
+export default function MapInput (props) {
 
-export default class MapInput extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-    
-    };
-  }
+    const [state, setState] = useState({
+      'start': '',
+      'end': '',
+      'minMax': '',
+      'percent': ''
+    });
+    function sendData(){
+      let values = {
+        start: props.state.start,
+        end: props.state.end,
+        minMax: state.minMax,
+        percent: state.percent,
+        done: true
+      }
+      props.onStateChange(values);
+    }
+  
 
-  render() {
     return (
         
       <div class = " flex bg-white-500 inset-x-0 top-0 text-center bg-orange-500 pb-1">
           <div class = "flex-1 bg-gray-300 mx-64 text-center rounded-lg">
             <label for = "startPoint" class = "ml-1"> Start Point: </label>
-            <input id = "startPoint" type = "text" class = "input-small inline-block text-center" placeholder = "Start Point: [lat, lng]"></input>
+            <input id = "startPoint" type = "text" class = "input-small inline-block text-center" placeholder = "Start Point: [lat, lng]" value = {props.state.start} onChange = {(e) => setState({start: e.target.value, end: state.end, minMax: state.minMax, percent: state.percent})}></input>
             <label for = "endPoint" > End Point: </label>
-            <input id = "endPoint" type = "text" class = "input-small inline-block text-center" placeholder = "End Point: [lat, lng]"></input>
+            <input id = "endPoint" type = "text" class = "input-small inline-block text-center" placeholder = "End Point: [lat, lng]" value = {props.state.end} onChange = {(e) => setState({start: state.start, end: e.target.value, minMax: state.minMax, percent: state.percent})}></input>
             <label for = "minMax"> Min or Max: </label>
-            <select id = "minMax" class="inline-block  text-center bg-white border border-gray-400 hover:border-gray-500 px-2 py-2 pr-1 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+            <select id = "minMax" class="inline-block  text-center bg-white border border-gray-400 hover:border-gray-500 px-2 py-2 pr-1 rounded shadow leading-tight focus:outline-none focus:shadow-outline" onChange = {(e) => setState({start: state.start, end: state.end, minMax: e.target.value, percent: state.percent})}>
                 <option>Minimize</option>
                 <option>Maximize</option>
             </select>
             <label for = "%" class = "ml-5"> x% of Path: </label>
-            <input id = "%" type="text" class = "input-small inline-block text-center" placeholder = "10%"/>
-            <button class = "button bg-black text-white border-black btn">Submit </button>
+            <input id = "%" type="text" class = "input-small inline-block text-center" placeholder = "10%" onChange = {(e) => setState({start: state.start, end: state.end, minMax: state.minMax, percent: e.target.value})}/>
+            <button class = "button bg-black text-white border-black btn" onClick = {sendData}>Submit </button>
             <br/>
             <small class = "block inset-x-0 top-0 text-center">Or can also click two points on the map</small>
             
@@ -35,6 +46,6 @@ export default class MapInput extends React.Component {
           
         
       </div>
-    );
-  }
+    )
+
 }
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -2,77 +2,20 @@ import Head from 'next/head'
 import React, { useEffect, useState } from 'react'
 import dynamic from 'next/dynamic'
 import axios from 'axios'
-
 const DynamicMap = dynamic(() => import('../components/map'), {ssr: false});
 const MapInput = dynamic(()=> import('../components/mapInput'), {ssr: false})
 
-let incIdx = (idx, length, setIdx) => {
-  let counter = (idx + 1) % length
-  setIdx(counter);
-}
+
 export default function Home(){
-  const [customers, setCustomers] = useState({})
-  let [idx, setIdx] = useState(0)
-  // const api = 'http://localhost:5000/api/users';
+  const[state, setState] = useState({'start': '', 'end': '', 'minMax':'', 'percent': '', 'done': false})
+  const[path, setPath] = useState([]);
 
-  // useEffect(() => {
 
-  //   const loadChar = async () => {
-  //     const result = await axios(api);
-  //     setCustomers(result.data);
-
-  //     incIdx(idx, result.data.length, setIdx);
-  //   };
-
-  //   loadChar();
-    
-  // }, [api]);
-
-  const mapped = []
-  for (let key in customers) {
-    // if (!customers.hasOwnProperty(key)) {
-    //   continue;
-    // }
-    let val = customers[key];
-
-    mapped.push(val);
-  }
-
-  const listItems = mapped.map((d,idx) => <li key={idx}>{[d.username]}</li>);
-  
   return (
     <div>
-      <DynamicMap />
+      <MapInput state = {state} onStateChange = {setState}></MapInput>
+      <DynamicMap state = {state} onStateChange = {setState}/>
     </div>
     
       )
-}
-
-function NameList(props) {
-  return props.mapped.map((d,idx) => {
-    let props = {
-      idx:idx,
-      d:d 
-    }
-    return <ListItem {...props}/>
-  });
-}
-  
-
-function ListItem(props) {
-  // console.log(`props${props}`)
-  const d = props.d;
-  const idx = props.idx;
-  // console.log(idx)
-  return (
-  <li key={idx}>{[d.customerName +
-    "\n" + "---", d.phone, 
-    "\n" + d.addressLine1,
-    "\n" + d.city, 
-    "\n" + d.state, 
-    "\n" + d.postalCode, 
-    "\n" + d.country, 
-    "\n" + d.creditLimit]}
-  </li> )
-
 }
