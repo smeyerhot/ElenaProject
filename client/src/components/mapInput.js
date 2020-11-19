09c4a9f124546031b242684ad4aa009eaea45b4f
Louis <louismartin@umass.edu>
2020-11-18 20:31:36 -0500

    Fixed no default value for elevation gain selector.

--- client/src/components/mapInput.js
+++ client/src/components/mapInput.js
@@ -6,7 +6,7 @@ export default function MapInput (props) {
     const [state, setState] = useState({
       'start': '',
       'end': '',
-      'minMax': '',
+      'minMax': 'Minimize',
       'percent': ''
     });
     function sendData(){
@@ -37,11 +37,11 @@ export default function MapInput (props) {
             <input id = "endPoint" type = "text" className = "input-small inline-block text-center" placeholder = "End Point: [lat, lng]" value = {props.state.end} onChange = {(e) => setState({start: state.start, end: e.target.value, minMax: state.minMax, percent: state.percent})}></input>
             <label for = "minMax" className = "font-bold"> Elevation Gain: </label>
             <select id = "minMax" class="inline-block  text-center bg-white border border-gray-400 hover:border-gray-500 px-2 py-2 pr-1 rounded shadow leading-tight focus:outline-none focus:shadow-outline" onChange = {(e) => setState({start: state.start, end: state.end, minMax: e.target.value, percent: state.percent})}>
-                <option>Minimize</option>
-                <option>Maximize</option>
+                <option value = 'Minimize' selected>Minimize</option>
+                <option value = 'Maximize'>Maximize</option>
             </select>
             <label for = "%" className = "ml-5 font-bold"> Path length (%): </label>
-            <span><input id = "%" type="number" className = "input-small inline-block text-center" placeholder = "1-200" min = '1' max = '200'onChange = {(e) => setState({start: state.start, end: state.end, minMax: state.minMax, percent: e.target.value})}/></span>
+            <span><input id = "%" type="number" className = "input-small inline-block text-center" placeholder = "100-400" min = '100' max = '400'onChange = {(e) => setState({start: state.start, end: state.end, minMax: state.minMax, percent: e.target.value})}/></span>
             <button class = "button bg-black text-white border-black btn" onClick = {sendData}>Calculate Path </button>
             <br/>
             <small class = "block inset-x-0 top-0 text-center font-bold">Click a start and an end point on the map, select to minimize or maximize elevation gain, input the maximum percentage of shortest path, then click "Calculate Path" to render your path.</small>
