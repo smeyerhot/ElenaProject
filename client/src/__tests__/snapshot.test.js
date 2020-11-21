0dad2c5880d6dcfdd62e414fc0992f04274f46fb
gwyneththoma <gwyneththoma@umass.edu>
2020-11-21 02:34:48 -0500

    skeleton tests and static statistics panel

--- client/src/__tests__/snapshot.test.js
+++ client/src/__tests__/snapshot.test.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import renderer from 'react-test-renderer'
 import Index from '../pages/index'
+import map from '../components/map'
 
 it('renders homepage unchanged', () => {
   const tree = renderer.create(<Index />).toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+it('renders route', () => {
+  const tree = renderer.create(<map/>).toJSON()
   expect(tree).toMatchSnapshot()
 })
